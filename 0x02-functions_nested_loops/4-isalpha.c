eaad80123bd32e0cbfe7b1d75b4a73c396444a87
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-13 22:47:38 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
 /**
- *_isalpha - controls if a character is an alphabet or not
- *@c: character to be verified
- *Return: return 0 or 1
+ * _isalpha - Entry point
+ * @c: the integer value it receives
+ * Description: checks if alphabet
+ * Return: 1 if true. 0 if false
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
