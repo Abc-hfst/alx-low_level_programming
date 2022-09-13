04015ba4d5b5efc39cd10cd07edcc36044ae9aa2
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-13 22:22:32 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * print_sign -function to checke sign number
- * @n: charcter to variefied
- *
- * Return: retun 0 or 1
+ *print_sign-prints sign of a number
+ *@n: character to e verified
+ *Return: return 0 or 1
  */
-
 int print_sign(int n)
 {
-	if (n > 0)
-	{
+	if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
+	else if (n > 0)
+{
 	_putchar('+');
 	return (1);
 }
-	elser if (n < 0)
-	{
+	else
+{
 	_putchar('-');
 	return (-1);
-	}
-	else
-	{
-	_putchar(0);
-	return (0);
-	}
+}
 }
