fdf399d1d45afeb7ed9f17f94758fde070af5a6d
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-13 22:56:50 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- *print_to_98 - Prints all natural numbers from n to 98
+ * print_to_98 - prints numbers to 98
+ * @n: starting integer
  *
- *@n: The number to check
+ * Description: prints all natural number from n - 98
+ *
+ * Return: void
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n < 98)
 	{
-		while (n >= 98)
+		while (n < 98)
 		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
-	}
-	else
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ", n);
 			n++;
 		}
 	}
-	printf("\n");
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
 }
