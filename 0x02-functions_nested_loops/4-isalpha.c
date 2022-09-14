73c5c0a9056ac7e2316bd367553bc483423e2001
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-14 14:42:54 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,10 @@
 #include "main.h"
-
 /**
- * _isalpha - Entry point
- * @c: the integer value it receives
- * Description: checks if alphabet
- * Return: 1 if true. 0 if false
- */
-
+*_isalpha -> checks for alphabetical letters
+*@c: a character to be checked on
+*Return: always returns 0
+*/
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-	return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
