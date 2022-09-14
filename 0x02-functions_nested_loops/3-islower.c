b80e29ad7394428602076ef75ee9672319d58c1f
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-14 14:44:24 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,10 @@
 #include "main.h"
-
 /**
- * _islower - Entry point
- * Description: checks is a character is lowercase
- * @c: the integer value it receives
- * Return: 1 if true. 0 if false.
+ * _islower -> checks if the character is lowercase
+ * @c: a character argument
+ * Return: always returns 0
  */
 int _islower(int c)
 {
-int i = 'a';
-
-for (i = 'a'; i <= 'z'; i++)
-{
-/* refer int c*/
-if (c == i)
-{
-return (1);
-}
-}
-return (0);
+return (c >= 'a' && c <= 'z');
 }
