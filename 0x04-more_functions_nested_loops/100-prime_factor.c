0c468307bc09d211f814012319388046f1847240
aminurabah@yahoo.com <aminurabah@yahoo.com>
2022-09-15 14:54:33 -0700

    advance more function task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	log n, i;
+
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
