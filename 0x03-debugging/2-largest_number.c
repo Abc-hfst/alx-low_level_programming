b788fd514b05468aa9417ddbc8b6f9ae91b35774
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-15 23:02:32 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number - returns the largest 0f 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: Largest number
+ * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -24,6 +22,5 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-
 	return (largest);
 }
