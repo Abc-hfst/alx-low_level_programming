51b01f2c01919546ebb496bc4b0d8ca322bfde05
Abc-hfst <111064677+Abc-hfst@users.noreply.github.com>
2022-09-20 02:13:09 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,23 @@
 #include "main.h"
+
 /**
- * _atoi - a function that converts a string to an integer
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - changes a string to an integer
+ * @s: the string to be changed
+ * Return: converted string to integer
  */
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int i = 1;
+	unsigned int num = 0;
 
-	while (s[c])
-	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isi == 1)
-		{
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
 			break;
-		}
-		c++;
-	}
-	ni *= min;
-	return (ni);
+	} while (*s++);
+
+	return (num * i);
 }
